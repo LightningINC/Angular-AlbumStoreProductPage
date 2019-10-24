184e2223aef16c95bf8b5344162c43dd4032771f
lightningINC <idowuolatunji3@gmail.com>
2019-10-24 06:19:49 +0100

    'casting'

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-import {map} from 'rxjs/operators';
+import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs/Observable';
 import { Album } from './album';
 
@@ -10,12 +10,6 @@ export class ProductService {
   constructor(private _http: Http) { }
 
   getAlbum(id: number): Observable<Album> {
-    return this._http.get(this._albumUrl).pipe(
-      map(response => {
-        const resp: Album = response.json();
-
-        return <Album>resp;
-      })
-    );
+    return this._http.get(this._albumUrl).map(response => <Album>response.json()  );
   }
 }
