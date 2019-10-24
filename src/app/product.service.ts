8e7076b488ab2227b1ce0399cfdd9ecf9246dabc
lightningINC <idowuolatunji3@gmail.com>
2019-10-24 06:05:53 +0100

    casting response

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -11,7 +11,7 @@ export class ProductService {
 
   getAlbum(id: number): Observable<Album> {
     return this._http.get(this._albumUrl).pipe(
-      map(response => <Album> response.json().album)
+      map(response => <Album>response.json())
     );
   }
 }
